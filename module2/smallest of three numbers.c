ddb645b720643817b8af10b58c4eaa061c192411
karthickvickram <37590459+karthickvickram@users.noreply.github.com>
2018-03-20 22:36:49 +0530

    Create smallest of three numbers.c

---
+++ module2/smallest of three numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n,i,n1,n2,n3,max1,max,min1,min2,min,med;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d %d %d\n",&n1,&n2,&n3);
+        max1=(n1>n2)?n1:n2;
+        min1=(n1<n2)?n1:n2;
+        max =(max1>n3)?max1:n3;
+        min2=(max1<n3)?max1:n3;
+        min =(min1<min2)?min1:min2;
+        med =(min1>min2)?min1:min2;
+        printf("%d < %d < %d\n",min,med,max);
+    }
+    return 0;
+}
